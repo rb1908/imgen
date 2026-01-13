48279cf33fb433f401d1ad1fc0badd5433dd9bd4
Rishabh Bhansali <rishabh@Rishabhs-MacBook-Air.local>
2026-01-13 16:07:05 +0530

    fix: Add postinstall generate and force dynamic home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import { getProjects } from '@/app/actions/projects';
-import { ProjectList } from '@/components/ProjectList';
+import { ProjectList } from "@/components/ProjectList";
+
+export const dynamic = 'force-dynamic';
 
 export default async function Home() {
     const projects = await getProjects();
